2a030aea4cfa585b36ee8930999d2cabc15f1b0a
Sunny Han <shan66@ucsc.edu>
2025-12-09 00:56:50 -0800

    changing name

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -8,7 +8,7 @@ import MarkdownPage from "../components/MarkdownPage";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/taste-without-trouble",
     element: <App />,
     children: [
       { path: "", element: <Home /> },
