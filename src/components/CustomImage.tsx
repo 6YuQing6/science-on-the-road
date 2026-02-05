b7c651e77797328490dada00ceefe40b88531fbd
Sunny Han <shan66@ucsc.edu>
2026-02-04 19:17:11 -0800

    image fix

--- src/components/CustomImage.tsx
+++ src/components/CustomImage.tsx
@@ -3,7 +3,6 @@ import React from "react";
 type CustomImageProps = React.ImgHTMLAttributes<HTMLImageElement>;
 
 const CustomImage: React.FC<CustomImageProps> = ({ src, alt, ...props }) => {
-  console.log("this is src", src);
   if (!src) return null;
   return <img src={src} alt={alt} className="markdown-img" {...props} />;
 };
